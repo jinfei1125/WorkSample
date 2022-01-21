21b09251d007bc65f65b8d3cdf750b0632a7b6a0
Jinfei Zhu <jinfei@uchicago.edu>
2022-01-21 15:03:02 -0600

    re-organized work sample

--- sqlcode/covid_big_query_by_state.sql
+++
@@ -1,4 +0,0 @@
-SELECT  province_state, country_region, date, SUM(confirmed) AS confirmed_state, SUM(deaths) AS deaths_state
-FROM `bigquery-public-data.covid19_jhu_csse.summary`
-WHERE country_region = 'US'
-GROUP BY province_state, date, country_region
