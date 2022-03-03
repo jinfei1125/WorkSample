1e35db11f4de294541f79dba70d4eacab9c15106
Jinfei Zhu <jinfei@uchicago.edu>
2022-03-03 09:16:57 -0600

    update practice SQL

--- SQL work sample/practice.sql
+++ SQL work sample/practice.sql
@@ -1,1 +1,27 @@
-SELECT
+SELECT student_id, sum(marks) as SUM_OF_MARKS
+FROM marks 
+GROUP BY student_id
+ORDER BY student_id DESC
+
+-- count duplicate name
+SELECT name, COUNT(CONCAT(name, phone, age)) as cnt
+GROUP BY name
+HAVING cnt > 1
+
+
+-- UNION
+SELECT * 
+FROM (SELECT DISTINCT city, LENGTH(city) 
+	FROM station 
+	ORDER BY LENGTH(city) ASC, city ASC
+	) 
+LIMIT 1
+
+UNION
+
+SELECT * 
+FROM 
+	(SELECT DISTINCT city, LENGTH(city) 
+		FROM station 
+		ORDER BY LENGTH(city) DESC, city ASC) 
+LIMIT 1
